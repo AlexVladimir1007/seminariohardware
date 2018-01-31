24d392f721cb05eda2a6cfe84179edb22bd04ba1
AlexVladimir1007 <alexkyu9@gmail.com>
2018-01-30 18:56:06 -0600

    agregar numero de cuenta

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+//mario alexander perez mendez 0801199410973
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Mario' });
+});
+
+module.exports = router;
